bf7bff813fc46579eac92a8df2c71a0ea8f1452c
Avishkar Parab <aviparab72733@gmail.com>
2022-10-21 02:33:56 +0530

    Added course files

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import Navbar from "./Navbar"
-import { FaEdit,FaPlusCircle } from "react-icons/fa";
+import { FaEdit,FaPlusCircle,FaTrashAlt } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios"
 
 const Course = () => {
 
-let navigate = useNavigate();
+var navigate = useNavigate();
+const [courses,setCourses] = useState([]);
+const [isDelete,setIsDelete] = useState(0)
+
+useEffect(() => {
+    const getcourse = async()=>{
+        try {
+            var response = await axios.get("/course");
+            console.log(response.data.result);
+            setCourses(response.data.result)
+
+        } catch (error) {
+            console.log(error);
+            setCourses("")
+
+        }
+    }
+    getcourse();
+}, [isDelete])
+
+async function deleteCourse(id){
+    try {
+        var response = await axios.delete(`/course/?id=${id}`);
+        if(response.data.result)
+            setIsDelete(1)
+            // navigate("/course");
+      } catch (error) {
+        console.log(error);
+      }
+}
+
   return (
     <>
       <Navbar/>
@@ -32,39 +63,44 @@ let navigate = useNavigate();
                     <th>Course Name</th>
                     <th>Total Module</th>
                     <th>Edit</th>
+                    <th>Delete</th>
                     <th>Add module</th>
                     </tr>
                 </thead>
                 <tbody>
+
+                {(!courses)?
                     <tr>
-                    <td>1</td>
-                    <td>Scratch</td>
-                    <td>10</td>
-                    <td className='d-flex justify-content-center align-items-center'> 
-                        <Button className='btn' variant="" type="button">
-                            <FaEdit className='fs-5 text-warning'/>
-                        </Button> </td>
-                    <td>
-                        <Button variant="" type="button">
-                            <FaPlusCircle className='fs-5 text-success'/>
-                        </Button>
-                    </td>
+                        <td>NO RESULTS FOUND</td>
                     </tr>
-                    <tr>
-                    <td>2</td>
-                    <td>Kojo</td>
-                    <td>10</td>
-                    <td className='d-flex justify-content-center align-items-center'> 
-                        <Button variant="" type="button">
-                            <FaEdit className='fs-5 text-warning'/>
-                        </Button> </td>
-                    <td>
-                        <Button variant="" type="button">
-                            <FaPlusCircle className='fs-5 text-success'/>
-                        </Button>
-                    </td>
-                    </tr>
-                    
+                    :
+                    courses.map((elem)=>{
+                       return(
+                        <tr key={elem.id}>
+                        <td>{elem.id}</td>
+                        <td>{elem.cname}</td>
+                        <td>{elem.tot_module}</td>
+                        <td className='d-flex justify-content-center align-items-center'> 
+                            <Button className='btn' variant="" type="button"
+                                onClick={()=>{navigate("/editcourse",{state:{course:elem}})}}>
+                                <FaEdit className='fs-5 text-warning'/>
+                            </Button> </td>
+                        <td>
+                            <Button variant="" type="button"
+                            onClick={()=>{deleteCourse(elem.id)}}>
+                                <FaTrashAlt className='fs-5 text-danger'/>
+                            </Button>
+                        </td>
+                        <td>
+                            <Button variant="" type="button"
+                            onClick={()=>{navigate("/addmodule",{state:{cid:elem.id}})}}>
+                                <FaPlusCircle className='fs-5 text-success'/>
+                            </Button>
+                        </td>
+                        </tr>
+                       )
+
+                    })}
                 </tbody>
             </Table>
 
